9b680507700f1ca5426f39868d771e0773c15096
szubarau <zumich92@gmail.com>
2025-06-21 22:25:19 +0300

    update 27

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 import time
+from config import Config
 from twitch_monitor import TwitchMonitor
 from telegram_notifier import TelegramNotifier
 
+
 def main():
+    config = Config()
     monitor = TwitchMonitor()
-    notifier = TelegramNotifier()
+    notifier = TelegramNotifier(config)
     streamer = "zumich"
 
     print("Бот запущен. Мониторинг стримов...")
@@ -14,7 +17,8 @@ def main():
             notifier.send_notification(stream_data)
             time.sleep(3600)  # Не спамить при долгих стримах
         else:
-            time.sleep(300)   # Проверка каждые 5 минут
+            time.sleep(300)  # Проверка каждые 5 минут
+
 
 if __name__ == "__main__":
     main()

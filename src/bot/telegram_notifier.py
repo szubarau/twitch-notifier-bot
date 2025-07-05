826d90b72b9ef8ed4236f1ddcd813fff551ecbcd
szubarau <zumich92@gmail.com>
2025-07-05 13:34:01 +0300

    Update 39

--- src/bot/telegram_notifier.py
+++ src/bot/telegram_notifier.py
@@ -11,13 +11,15 @@ class TelegramNotifier:
     def __init__(self, config):
         self.config = config
         self.bot = Bot(token=self.config.telegram_token)
-        self.id_file = "last_stream_id.txt"
+
+        # Пусть до артефакта или файл, который будет сохраняться между запусками
+        self.id_file = os.path.join(os.getcwd(), "last_stream_id.txt")
 
     async def send_if_new(self, stream_data: dict):
         stream_id = str(stream_data.get("id"))
-        if self.should_abort_pipeline(stream_id):
-            logging.info("🛑 Стрим уже был анонсирован. Завершаем пайплайн.")
-            sys.exit(0)  # Прерывание пайплайна без ошибок
+        if self._get_last_sent_id() == stream_id:
+            logging.info("🛑 Стрим уже был анонсирован. Прерываем пайплайн.")
+            sys.exit(0)  # Успешный выход, останавливаем Pipeline
 
         await self.send_announcement_with_image(stream_data)
         self._save_last_sent_id(stream_id)
@@ -30,7 +32,7 @@ class TelegramNotifier:
         captions = [
             f"🎬 <b>Стрим начался!</b>\n\n📺 <b>{title}</b>\n🎮 Игра: <b>{game}</b>\n<a href='https://twitch.tv/zumich'>▶️ Смотреть</a>",
             f"🔥 <b>Zumich в эфире!</b>\n\n🎮 <b>{game}</b>\n🗣 <b>{title}</b>\n<a href='https://twitch.tv/zumich'>🔴 Присоединяйся</a>",
-            f"🚨 <b>Прямой эфир уже идёт!</b>\n\n🎲 <b>{game}</b>\n🎙 <b>{title}</b>\n▶️ <a href='https://twitch.tv/zumich'>Смотри на Twitch</a>",
+            f"🚨 <b>Прямой эфир!</b>\n\n🎲 <b>{game}</b>\n🎙 <b>{title}</b>\n▶️ <a href='https://twitch.tv/zumich'>Смотри на Twitch</a>",
         ]
         caption = random.choice(captions)
 
@@ -41,13 +43,9 @@ class TelegramNotifier:
                 caption=caption,
                 parse_mode=ParseMode.HTML
             )
-            logging.info("✅ Анонс отправлен с обложкой.")
+            logging.info("✅ Анонс с превью отправлен.")
         except Exception as e:
             logging.error(f"❌ Ошибка при отправке фото: {e}")
-
-    def should_abort_pipeline(self, stream_id: str) -> bool:
-        last_id = self._get_last_sent_id()
-        return last_id == stream_id
 
     def _get_last_sent_id(self) -> Optional[str]:
         if os.path.exists(self.id_file):
@@ -56,6 +54,10 @@ class TelegramNotifier:
         return None
 
     def _save_last_sent_id(self, stream_id: str):
-        with open(self.id_file, "w") as f:
-            f.write(stream_id)
+        try:
+            with open(self.id_file, "w") as f:
+                f.write(stream_id)
+        except Exception as e:
+            logging.error(f"❌ Не удалось сохранить ID стрима: {e}")
+
 
